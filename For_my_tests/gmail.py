ef9fdb6dab65c5d4f655cb98f989378f6d0c57ef
alendina <alendin@gmail.com>
2022-01-03 21:03:55 +0200

    test: delete folder .idea

--- For_my_tests/gmail.py
+++ For_my_tests/gmail.py
@@ -7,7 +7,7 @@ browser = webdriver.Chrome()
 try:
     browser.get(link)
     sleep(1)
-
+    # WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), '$100'))
 finally:
     sleep(3)
     browser.quit()
